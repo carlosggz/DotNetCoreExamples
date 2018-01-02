602962fa35f630017b0a4d22fd9b429273b14bef
Carlos Garcia <carlosggz@gmail.com>
2018-01-02 15:36:43 +0100

    IoC

---
+++ IoC/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IoC.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IoC.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly IMyService _service = null;
+
+        public ValuesController(IMyService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return _service.GetDate();
+        }
+    }
+}
---
+++ IoC/Services/MyService.cs
@@ -0,0 +1,16 @@
+﻿using IoC.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoC.Services
+{
+    public class MyService : IMyService
+    {
+        public string GetDate()
+        {
+            return System.DateTime.Now.ToLongDateString();
+        }
+    }
+}
