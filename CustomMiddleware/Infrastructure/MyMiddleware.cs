f9d85ce2a6200c76eb78d8308a274a251bc2a6ff
Carlos Garcia <carlosggz@gmail.com>
2018-01-02 15:30:02 +0100

    Custom Middleware

---
+++ CustomMiddleware/Infrastructure/MyMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomMiddleware.Infrastructure
+{
+    public class MyMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public MyMiddleware(RequestDelegate next)
+        {
+            nextDelegate = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Path.ToString().ToLower() == "/my")
+            {
+                await httpContext
+                    .Response
+                    .WriteAsync("Hi from your middleware", Encoding.UTF8);
+            }
+            else
+            {
+                await nextDelegate.Invoke(httpContext);
+            }
+        }
+    }
+}
